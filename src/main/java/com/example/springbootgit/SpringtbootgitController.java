9482fa541fa42f688093e21c70a34fb088985f98
vivek mishra <mishravivek@microsoft.com>
2023-09-06 17:14:29 -0500

    first commit

---
+++ src/main/java/com/example/springbootgit/SpringtbootgitController.java
@@ -0,0 +1,15 @@
+package com.example.springbootgit;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class SpringtbootgitController {
+
+    // help me to create a get method that return string
+    @GetMapping
+    public String successMessage(){
+        return "Hello GIT";
+    }
+}
